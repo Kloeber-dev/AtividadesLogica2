b7c6f012db3df75d4d80c96427da726874818528
Kleber Gonçalves de Souza <71834748+Kloeber-dev@users.noreply.github.com>
2021-02-10 14:12:48 -0300

    Exercícios sobre Fila

---
+++ aula_fila/aula_fila/TesteDaFila.java
@@ -0,0 +1,30 @@
+package aula_fila;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TesteDaFila {
+	
+	public static void main(String[] args) {
+		
+		Fila fila = new Fila();
+		
+		fila.adiciona("Maurício");
+		fila.adiciona("Guilherme");
+		
+		System.out.println(fila);
+		
+		String x1 = fila.remove();
+		
+		System.out.println(x1);
+		System.out.println(fila);
+		
+		Queue<String> filaDoJava = new LinkedList<String>();
+		
+		filaDoJava.add("Maurício");
+		String x2 = filaDoJava.poll();
+		
+		System.out.println(x2);
+	}
+
+}
