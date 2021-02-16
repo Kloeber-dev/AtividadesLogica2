363ef4c4cec0e9f5a8f82aeeb1093a7d5f84a7f2
Kleber Gonçalves de Souza <71834748+Kloeber-dev@users.noreply.github.com>
2021-02-16 17:24:03 -0300

    Exercícios Algoritmos de Ordenação e Recursão

---
+++ aula_AlgoritmosOrdenacao/Teste.java
@@ -0,0 +1,15 @@
+package aula_AlgoritmosOrdenacao;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		int fatorialNR = Recursividade.fatorialNaoRecursivo(5);
+		System.out.println(fatorialNR);
+		
+		int fatorialR = Recursividade.fatorial(5);
+		System.out.println(fatorialR);
+
+	}
+
+}
